fac46e115c98254c3c86277f80e55e02d772ffa7
JonasRede <JonasRede@web.de>
2021-07-15 17:38:51 +0200

    socket.disconnect und auskommentiere commit

--- bin/server.js
+++ bin/server.js
@@ -15,19 +15,19 @@ var server = require('http').createServer(app)
 const port = normalizePort(process.env.PORT || '9000')
 app.set('port', port)
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://shrijk.netlify.app/");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.header("Access-Control-Allow-Credentials", true);
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://shrijk.netlify.app/");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(204);
+//   }
+//   next();
+// });
 
 // Listen on provided port, on all network interfaces.
 server.listen(port, () => {
@@ -139,7 +139,7 @@ io.on('connection', socket => {
 	socket.on('leave_room', () => {
 		console.log('> LEAVE\n')
 		socket.disconnect()
-		socket.socket.reconnect()
+		// socket.socket.reconnect()
 		// // we suffer already. may as well use our pain
 		// leave(socket.id)
 		// let roomcode = pain.get(socket.id)
